e7ccb3755c10f43b7a09ddd97b7fb22a59721a2c
hamalxd <sammanhamal06@gmail.com>
2022-12-13 17:45:06 +0545

    js day 1

---
+++ day5.js
@@ -0,0 +1,43 @@
+// declaring variables
+// var a = 1;
+// can be reassigned and cannot be re-declared
+// b = 3;
+// cannot reassign and re-declare value 
+// const c = 5;
+// alert(a + b);
+// let a = 10;
+// let name = 'hamal';
+// let test;
+// alert(test)// not assigned value = undefined 
+//prompt(test) can take input
+// console.log ("Hello World");
+
+// let a = 10;
+// let b = 20;
+
+// z = (a == b);
+// == is used to check the value only.  === is used to check  the value and datatype.
+// alert(z);
+//  let isLoggedIn = true;
+//  let isSuperUser = true;
+ //logical and
+//  let t = isLoggedIn && isSuperUser;
+ //logical or
+//  let test = true || false;
+ //not
+//  let test1 = !true;
+//  alert(test1);
+
+ //array
+//  let array = [1,2,3,4,5,6,7];
+//  alert(array[2]);
+
+//object
+//let object = {name: "hamal",age: 17};
+//                key:value
+// console.log(object.name, "object");
+// console.log (object["age"], "object");  
+//object[key] or object.key
+
+let object1 = {name: "Hamal", location: "new baneshwor", College: "ISMT"};
+console.log(object1);
