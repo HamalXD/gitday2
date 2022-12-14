331586cd6732612769b57ecb14775f5bea44c022
hamalxd <sammanhamal06@gmail.com>
2022-12-14 16:52:08 +0545

    gitday3

---
+++ day 6.js
@@ -0,0 +1,27 @@
+// person1.name equals to my name
+// alert('I am Hamal');
+// alert();
+
+// let person1 = {
+//     firstName: 'Samman Hamal',
+//     secondName: 'Kismat Shrestha',
+// }
+
+// if (person1.firstName == 'Samman Hamal')
+//  alert('I am ' + person1.firstName);
+
+// else {
+//     alert('Error');
+// }
+
+
+const person = {
+    personName:'Hamal', age: 24
+};
+
+if (person.age > 18)
+alert('My age is' + " " + person.age);
+
+else {
+    alert('Error');
+}
---
+++ java ass.js
@@ -0,0 +1,13 @@
+let object = {
+    Name:"Hamal",
+    Location: "New Baneshwor",
+    College: "ISMT"
+}
+
+console.log(object)
+// console.log(object.Name)
+// console.log(object.Location)
+// console.log(object.College)
+
+ let array = [1,2,3];
+ console.log(array)
